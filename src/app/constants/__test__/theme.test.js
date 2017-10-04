9c1a478190039d5316e324a90aae950c044b8354
Vinod Ronold <vinodronoldbe@gmail.com>
2017-10-04 17:35:09 -0400

    Updated Theme tests

--- src/app/constants/__test__/theme.test.js
+++ src/app/constants/__test__/theme.test.js
@@ -4,8 +4,40 @@ describe('theme', () => {
   it('mobile should be 768', () => {
     expect(theme.mobile).toBe(768)
   })
-  
+
   it('tablet should be 992', () => {
     expect(theme.tablet).toBe(992)
+  })
+
+  it('Base font should be 16px', () => {
+    expect(theme.baseFont).toBe('16px')
+  })
+
+  it('Font Family should be Helvetica, Arial, sans-serif', () => {
+    expect(theme.baseFont).toBe('Helvetica, Arial, sans-serif')
+  })
+
+  it('Primary Color should be rgba(255,255,255,1)', () => {
+    expect(theme.color.primary()).toBe('rgba(255,255,255,1)')
+  })
+
+  it('Primary Color opacity should be updated', () => {
+    expect(theme.color.primary(0.5)).toBe('rgba(255,255,255,0.5)')
+  })
+
+  it('Secondary Color should be rgba(251,192,45,0.5)', () => {
+    expect(theme.color.secondary()).toBe('rgba(251,192,45,0.5)')
+  })
+
+  it('Secondary Color opacity should be updated', () => {
+    expect(theme.color.secondary(1)).toBe('rgba(251,192,45,1)')
+  })
+
+  it('Background Color should be rgba(0,0,0,0.8)', () => {
+    expect(theme.color.bg()).toBe('rgba(0,0,0,0.8)')
+  })
+
+  it('Background Color opacity should be updated', () => {
+    expect(theme.color.bg(1)).toBe('rgba(0,0,0,1)')
   })
 })
--- src/app/constants/theme.js
+++ src/app/constants/theme.js
@@ -2,11 +2,8 @@ export const mobile = 768
 export const tablet = 992
 export const color = {
   primary: (o = 1) => `rgba(255,255,255,${o})`,
-  secondary: (o = 0.5) => `rgba(251, 192, 45,${o})`,
+  secondary: (o = 0.5) => `rgba(251,192,45,${o})`,
   bg: (o = 0.8) => `rgba(0,0,0,${o})`
 }
 export const baseFont = '16px'
 export const fontFamily = `Helvetica, Arial, sans-serif`
-export const media = {
-  mobile: `@media only screen and (max-width: ${mobile}px)`
-}
